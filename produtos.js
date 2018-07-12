d6bfcbd932d2d7259033345441acd16de85e02f1
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-12 19:02:18 -0300

    js removido

--- produtos.js
+++ produtos.js
@@ -1,1 +1,10 @@
-console.log("listagem de produtos");
+var http = require('http');
+
+var server = http.createServer(function(request,response){
+    if(request.url == "/produtos"){
+        response.end("<html><body><h1>Listando os produtos</h1></body></html>");
+    }else{
+        response.end("<html><body><h1>Home</h1></body></html>")
+    }  
+}).listen(3000,"localhost");
+console.log("servidor ta rodando");
