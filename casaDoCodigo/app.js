cb41b977aa98d5f72562f8a85948a5e3bebe01f0
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-12 20:56:56 -0300

    adição do EJS

--- casaDoCodigo/app.js
+++ casaDoCodigo/app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+app.set('view engine','ejs');
 app.get('/produtos',function(req,res){
-    res.send("<hmtl><body><h1>Listagm de produtos</h1></body></html>")
+    res.render("produtos/lista");
 });
 app.listen(3000,function(){
     console.log("seridor rodando");
