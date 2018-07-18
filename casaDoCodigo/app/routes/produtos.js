beee25f24ff35005e10faf7439fd01c4d69f1971
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-18 19:41:40 -0300

    adição do express-load

--- casaDoCodigo/app/routes/produtos.js
+++ casaDoCodigo/app/routes/produtos.js
@@ -1,9 +1,8 @@
-var connectionFactory = require('../infra/ConnectionFactory');
+
 module.exports = function(app){
   app.get('/produtos',function(req,res){
 
-      var connection = connectionFactory();
-
+      var connection = app.infra.ConnectionFactory();
       connection.query('select * from livro',function(err,results){
           res.render('produtos/lista', {lista:results});
       });
