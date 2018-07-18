a95f5e895e66f2f421790e78dca0831772819200
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-18 00:15:32 -0300

    primeira consulta nobanco de dados

--- casaDoCodigo/app/routes/produtos.js
+++ casaDoCodigo/app/routes/produtos.js
@@ -1,17 +1,17 @@
 
 module.exports = function(app){
 app.get('/produtos',function(req,res){
-    var mysql = require('mysql');
+    var mysql =  require('mysql');
     var connection = mysql.createConnection({
         host:'localhost',
         user:'root',
         password:'',
         database:'node'
     });
-    connection.query('select * from livros',function(err,results){
-        res.send(results);
+    connection.query('select * from livro',function(err,results){
+    res.send(results);
     });
     connection.end();
-    res.render("produtos/lista");
-});
-}
+    
+    });
+} 
