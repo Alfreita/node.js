5fc5a4e0c8cf6f7f264eb08f1cb2fe819c57538e
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-18 08:40:49 -0300

    mudança no html

--- casaDoCodigo/app/routes/produtos.js
+++ casaDoCodigo/app/routes/produtos.js
@@ -9,7 +9,7 @@ app.get('/produtos',function(req,res){
         database:'node'
     });
     connection.query('select * from livro',function(err,results){
-    res.send(results);
+    res.render('produtos/lista',{lista:reslts});
     });
     connection.end();
     
