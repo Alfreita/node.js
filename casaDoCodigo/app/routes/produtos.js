1371bed580a82017b616e9be9b49812d51e9da71
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-18 19:06:30 -0300

    connectionFactory do mysql

--- casaDoCodigo/app/routes/produtos.js
+++ casaDoCodigo/app/routes/produtos.js
@@ -1,8 +1,8 @@
-var dbConnection = require('../infra/dbConnection');
+var connectionFactory = require('../infra/ConnectionFactory');
 module.exports = function(app){
   app.get('/produtos',function(req,res){
 
-      var connection = dbConnection();
+      var connection = connectionFactory();
 
       connection.query('select * from livro',function(err,results){
           res.render('produtos/lista', {lista:results});
