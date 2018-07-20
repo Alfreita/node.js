295c1bfeb12e1e8005786521dfcd341ee50178ca
Victor Almeida De Freitas <victor.freitas08@hotmail.com>
2018-07-20 09:57:23 -0300

    mudanças na remove

--- casaDoCodigo/app/routes/removeProduto.js
+++ casaDoCodigo/app/routes/removeProduto.js
@@ -1,4 +1,11 @@
-module.exports  = function(app){
-    app.get('/removeproduto')
 
-}
+    app.get('/removeproduto',function(){
+    var connection = app.infr.Con;nectionFactory;
+    var produtos = app.infra.produtosBanco;
+    var produto = produtosBanco.carrega(connection,id,callback);
+
+    if(produto){
+        produtosBanco.remove(connection,produto,callback);
+
+    }
+});
